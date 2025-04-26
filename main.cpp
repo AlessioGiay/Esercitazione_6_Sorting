852f5202a1b9fcc32513e45c159e37e36d79204b
Alessio Giay <s310703@studenti.polito.it>
2025-04-26 18:50:18 +0200

    Primo Upload

--- main.cpp
+++ main.cpp
@@ -1,5 +1,56 @@
-int main()
+#include <numeric>
+#include <chrono>
+#include "SortingAlgorithm.hpp"
+
+using namespace std;
+using namespace chrono;
+
+int main(int argc, char* argv[])
 {
-    return 0;
+	if (argc != 2)
+	{
+		cerr << "Scrivi: " << argv[0] << " dimensione\n";
+	}
+	
+	const unsigned int size = stoi(argv[1]);
+	const unsigned int Simulation = 25;
+		
+	vector<unsigned int> v(size);
+	
+	iota(v.begin(), v.end(), 1);
+	random_shuffle(v.begin(), v.end());
+	
+	double BubbleTime = 0;
+	for(size_t i = 0; i < Simulation; i++)
+	{
+		vector<unsigned int> copy = v;
+		auto start = steady_clock::now();
+		SortLibrary::BubbleSort(copy);
+		auto end = steady_clock::now();
+		
+		BubbleTime += duration_cast<microseconds>(end - start).count();
+	}
+	
+	BubbleTime /= Simulation;
+	
+	
+	double HeapTime = 0;
+	for(size_t i = 0; i < Simulation; i++)
+	{
+		vector<unsigned int> copy = v;
+		auto start = steady_clock::now();
+		SortLibrary::HeapSort(copy);
+		auto end = steady_clock::now();
+		
+		HeapTime += duration_cast<microseconds>(end - start).count();
+	}
+	
+	HeapTime /= Simulation;
+	
+	
+	cout<< "BubbleSort: "<<BubbleTime<<" μs"<<endl;
+	cout<< "HeapSort: "<<HeapTime<<" μs"<<endl;
+	
+	return 0;
 }
 
--- src/SortingAlgorithm.hpp
+++ src/SortingAlgorithm.hpp
@@ -5,9 +5,59 @@
 
 using namespace std;
 
-namespace SortLibrary {
+namespace SortLibrary 
+{
+	template<typename T>
+	
+	void  BubbleSort(vector<T>& v)
+	{
+		const unsigned int n = v.size();
+		
+		for(size_t i = 0; i < n-1; i++)
+		{
+			for(size_t j = i+1; j < n; j++)
+			{
+				if(v[j] < v[i])
+				{
+					const T tmp = v[i];
+					v[i] = v[j];
+					v[j] = tmp;
+				}
+			}
+		}
+	}
 
-
+	template<typename T>
+	
+	void HeapSort(vector<T>& v)
+	{
+		unsigned int n = v.size();
+		
+		while(n)
+		{
+			for(size_t son = 1; son < n; son++)
+			{
+				unsigned int father = (son-1)/2;
+				
+				while(v[father] < v[son])
+				{
+					const T tmp = v[son];
+					v[son] = v[father];
+					v[father] = tmp;
+					
+					if(father!=0)
+					{
+						son = father;
+						father = (son-1)/2;
+					}
+				}
+			}
+			
+			T tmp = v[n-1];
+			v[n-1] = v[0];
+			v[0] = tmp;
+			
+			n--;
+		}
+	}			
 }
-
-
